3c602c70418abfd9c02da62619e43a5bbe717f33
Nevena Fundup <nevenafundup7@gmail.com>
2022-09-21 15:03:39 +0200

    dodate slike i kartica.php

---
+++ kartica.php
@@ -0,0 +1,15 @@
+<div class="col-sm">
+    <form action="" method="post">
+        <img class="card-img-top" src="slike/1.jpg" alt="slikaTretmana"> 
+        <h5 class="card-title"   name="naziv"> </h5>  
+        <p class="card-text"   name="opis"></p>  
+        <p class="card-text"   name="cena"></p>                                                    
+        <p class="card-text"   name="lokacija"></p> 
+     </form>
+</div> 
+
+
+                   
+
+
+ 
